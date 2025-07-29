506fc40378720a8121c6ad1bab759dfc7b67594a
sharadkalya <sharadkalya@gmail.com>
2025-07-29 09:58:38 +0530

    :wrench: backend build working locally

--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { Router, Router as ExpressRouter } from 'express';
 
 import { login, logout, signup } from '../controllers/auth.controller';
 
-const router = Router();
+const router: ExpressRouter = Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
--- apps/backend/src/routes/dashboard.routes.ts
+++ apps/backend/src/routes/dashboard.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { getDashboardData } from '@/controllers/dashboard.controller';
 
-const router = Router();
+const router: Router = Router();
 router.get('/', getDashboardData);
 
 export default router;
