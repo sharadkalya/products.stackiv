5f300c69c3a3c6e43115d8df68c3ed4f07bdab79
sharadkalya <sharadkalya@gmail.com>
2025-11-26 16:28:44 +0530

    better auth in next app

--- apps/hosts/src/app/login/page.tsx
+++ apps/hosts/src/app/login/page.tsx
@@ -1,38 +1,35 @@
 import { getServerTranslation } from 'shared-i18n/server';
 
-import { AuthRedirect } from '@hosts/components/AuthRedirect';
-
 import EmailLogin from './components/EmailLogin';
 import PhoneLogin from './components/PhoneLogin';
 
 export default async function Login() {
     const { t } = await getServerTranslation('en');
-    return (
-        <AuthRedirect>
-            <div className="loginSignupPage">
-                <div className="tabs tabs-lift flex">
-                    <input
-                        type="radio"
-                        name="loginMethod"
-                        className="tab w-1/2"
-                        aria-label={t('loginWithEmail')}
-                        defaultChecked
-                    />
-                    <div className="tab-content bg-base-100 border-base-300 p-6">
-                        <EmailLogin />
-                    </div>
 
-                    <input
-                        type="radio"
-                        name="loginMethod"
-                        className="tab w-1/2"
-                        aria-label={t('loginWithPhone')}
-                    />
-                    <div className="tab-content bg-base-100 border-base-300 p-6">
-                        <PhoneLogin />
-                    </div>
+    return (
+        <div className="loginSignupPage">
+            <div className="tabs tabs-lift flex">
+                <input
+                    type="radio"
+                    name="loginMethod"
+                    className="tab w-1/2"
+                    aria-label={t('loginWithEmail')}
+                    defaultChecked
+                />
+                <div className="tab-content bg-base-100 border-base-300 p-6">
+                    <EmailLogin />
+                </div>
+
+                <input
+                    type="radio"
+                    name="loginMethod"
+                    className="tab w-1/2"
+                    aria-label={t('loginWithPhone')}
+                />
+                <div className="tab-content bg-base-100 border-base-300 p-6">
+                    <PhoneLogin />
                 </div>
             </div>
-        </AuthRedirect>
+        </div>
     );
 }
